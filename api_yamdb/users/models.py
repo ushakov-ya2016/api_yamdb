8139a80671eaaef0f4a866f70a72dabfaf6a9301
Иван Куприянов <ivan.kupryanoff@yandex.ru>
2022-09-07 17:09:36 +0300

    users1

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    USER_ROLES = (
+        ('user', 'user'),
+        ('moderator', 'moderator'),
+        ('admin', 'admin'),
+    )
+    
+    bio = models.TextField(
+        max_length=300,
+        blank=True,
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=USER_ROLES,
+        default='user',
+    )
